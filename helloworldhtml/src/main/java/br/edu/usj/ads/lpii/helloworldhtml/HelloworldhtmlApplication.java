f5d97afbc4e2905187acd62f213e26a82504b372
Marcos José Nazário <mjnazario2011@gmail.com>
2020-10-16 23:28:47 -0300

    Primeiro commit

---
+++ helloworldhtml/src/main/java/br/edu/usj/ads/lpii/helloworldhtml/HelloworldhtmlApplication.java
@@ -0,0 +1,13 @@
+package br.edu.usj.ads.lpii.helloworldhtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldhtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldhtmlApplication.class, args);
+	}
+
+}
